c855928fa9d439692603df655786e2ddedb43bb3
bspark6 <bspark6@lotte.net>
2020-08-31 18:03:09 +0900

    Test

--- lib/C.dart
+++ lib/C.dart
@@ -3,5 +3,5 @@ import 'dart:developer';
 var TAG = "Break Down";
 
 void logger(String msg) {
-  log("${TAG} (${DateTime.now().toIso8601String()} : ${msg})");
+  log("${TAG}|(${DateTime.now().toIso8601String()} : ${msg})");
 }
